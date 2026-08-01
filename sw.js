0a1398bb7f87137ba6aff3858869abd5f93601af
Murat Sönmez <11619503+m-sonmez@users.noreply.github.com>
2026-08-01 22:49:50 +0300

    added data

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@
 
 /* Service Worker Template */
 /* This file is processed by bump-sw.js to generate a versioned sw.js. */
-/* The medilog_2026-08-01_11-00-00 placeholder is replaced with a timestamp. */
+/* The medilog_2026-08-01_22-49-50 placeholder is replaced with a timestamp. */
 
-const CACHE_NAME = 'medilog_2026-08-01_11-00-00';
+const CACHE_NAME = 'medilog_2026-08-01_22-49-50';
 
 /* Static assets that are part of the app shell. */
 const STATIC_ASSETS = ['/panel.html', '/rapor.html', '/dist/output.css', '/dist/output.js', '/app.js', '/dist/icons/manifest.json', '/dist/icons/browserconfig.xml', '/dist/icons/favicon.ico'];
